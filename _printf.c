6bc5d4f2a30cec382b72479c591d65570ac76c12
Kariuki33 <danqarees@gmail.com>
2022-10-21 02:59:05 -0700

    Team Project

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -40,9 +40,6 @@ int _printf(const char *format, ...)
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
