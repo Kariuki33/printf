d0b46fa658b778438e80101f86625b250c3bc03a
Kariuki33 <danqarees@gmail.com>
2022-10-22 02:06:23 -0700

    Team Project

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
@@ -8,7 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * Return: Printed chars.
  */
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -62,7 +61,6 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-
 	*buff_ind = 0;
 }
 			
